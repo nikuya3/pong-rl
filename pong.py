6776bcc3cae99ea0430d0f3fe3464400e88d104d
nikuya3 <nikuya@dummy.mail>
2017-10-05 22:22:36 +0200

    Fixed bugs in forward and backward pass

--- pong.py
+++ pong.py
@@ -26,10 +26,12 @@ class PolicyNetwork:
     def __init__(self, input_size, hidden_sizes, out_size, eta):
         w_in = init_weight(input_size, hidden_sizes[0])
         self.weights.append(w_in)
-        for nr in range(1, len(hidden_sizes) - 1):
-            self.hidden_layers.append(np.empty(hidden_sizes[nr]))
+        #self.hidden_layers.append(np.empty(hidden_sizes[0]))
+        for nr in range(1, len(hidden_sizes)):
+            #self.hidden_layers.append(np.empty(hidden_sizes[nr]))
             w = init_weight(hidden_sizes[nr - 1], hidden_sizes[nr])
             self.weights.append(w)
+        #self.hidden_layers.append(np.empty(hidden_sizes[-1]))
         w_out = init_weight(hidden_sizes[-1], out_size)
         self.weights.append(w_out)
         self.out_size = out_size
@@ -38,28 +40,28 @@ class PolicyNetwork:
     def forward_pass(self, x):
         h_in = x.dot(self.weights[0])
         h_in = activation_function(h_in)
-        self.hidden_layers.append(h_in)
+        hs = [h_in]
         for nr in range(1, len(self.weights) - 1):
-            h = self.hidden_layers[nr - 1].dot(self.weights[nr])
+            h = self.weights[nr].dot(hs[nr - 1])
             h = activation_function(h)
-            self.hidden_layers.append(h)
-        scores = self.hidden_layers[-1].dot(self.weights[-1])
+            hs.append(h)
+        self.hidden_layers.append(hs)
+        scores = hs[-1].dot(self.weights[-1])
         return scores
 
-    def backward_pass(self, x, dscores):
-        x = x.reshape(len(x), 1)
-        dscores = dscores.reshape(len(dscores), 1)
-        dw_out = dscores.dot(self.hidden_layers[-1].T)
+    def backward_pass(self, nr, x, dscores):
+        dw_out = np.outer(dscores, self.hidden_layers[nr][-1].T)
         dws = [dw_out]
-        dhiddens = [self.weights[-1].T.dot(dscores)]
-        if len(self.hidden_layers) > 1:
-            for i in range(len(self.hidden_layers) - 1, -1, -1):
-                dhidden = dhiddens[-1].dot(self.weights[i + 1].T)
-                dhidden = activation_function_gradient(dhidden, self.hidden_layers[i])
+        dhidden = np.dot(dscores, self.weights[-1].T)
+        dhiddens = [dhidden]
+        if len(self.hidden_layers[nr]) > 1:
+            for i in range(len(self.hidden_layers[nr]) - 1, -1, -1):
+                dhidden = np.dot(dhiddens[-1], self.weights[i].T)
+                dhidden = activation_function_gradient(dhidden, self.hidden_layers[nr][i])
                 if i == 0:
                     dw = x.T.dot(dhidden)
                 else:
-                    dw = self.hidden_layers[i - 1].T.dot(dhidden)
+                    dw = self.hidden_layers[nr][i - 1].T.dot(dhidden)
                 dhiddens.append(dhidden)
                 dws.append(dw)
         self.weight_grads.append(dws)
@@ -80,55 +82,14 @@ def preprocess_image(image):
     return image
 
 
-def probs(scores):
-    """
-    Calculates the probabilities out of a neural networks class scores.
-    :param scores: The score matrix of form (N x K), where N is the number of observations and K is the number of classes.
-    :return: The probabilities of the same form as the input scores.
-    """
-    exp_scores = np.exp(scores)
-    return exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
-
-
-def calculate_cross_entropy_loss(s, y, w, lambda_):
-    """
-    Calculates the loss of a score matrix depending on the ground truth labels.
-    This method uses cross entropy loss (from Softmax).
-    :param s: The score matrix of form (N x K), where N is the number of observations and K is the number of classes.
-    :param y: The ground truth label vector of length N.
-    :param w: The weight matrix of the output layer of form (H x K), where H is the size of the previous layer.
-    :param lambda_: The regularization loss hyperparameter.
-    :return: The cross-entropy loss, where 0 indicates a perfect match between s and y
-    and +Inf indicates a perfect mismatch.
-    """
-    probabilities = probs(s)
-    log_probabilities = - np.log(probabilities[range(len(y)), y])
-    data_loss = np.sum(log_probabilities) / len(y)
-    return data_loss
-
-
-def cross_entropy_loss_gradient(s, y):
-    """
-    Calculates the gradient of the hinge loss function by the scores.
-    The gradient formula is { ds_j / dL = e^s_j / sum e^j, ds_y_i / dL = e^s_y_i / sum e^j - 1 }.
-    :param s: The score parameter of the loss function.
-    :param y: The ground truth label parameter of the loss function.
-    :return: The gradient as a matrix of the same shape as `s`.
-    """
-    dscores = probs(s)
-    dscores[range(len(y)), y] -= 1
-    dscores /= len(y)
-    return dscores
-
-
 def train(env):
     env.reset()
-    env.render()
     input_size = 80 * 80
     hidden_sizes = [200, 200]
     out_size = 3
     eta = 1e-4
     gamma = 0.99
+    render = False
     rolluts = 20
     actions = [0]
     observations = []
@@ -137,7 +98,8 @@ def train(env):
     while rolluts > 0:
         action = actions[-1] + 1
         observation, reward, done, info = env.step(action)
-        env.render()
+        if render:
+            env.render()
         observation = preprocess_image(observation)
         observations.append(observation)
         scores = net.forward_pass(observation)
@@ -150,7 +112,7 @@ def train(env):
         if done:
             for i in range(len(current_gradients)):
                 current_gradients[i] = current_gradients[i] * reward
-                net.backward_pass(observations[i], current_gradients[i])
+                net.backward_pass(i, observations[i], current_gradients[i])
             net.update_parameters()
             current_gradients = []
             observations = []
