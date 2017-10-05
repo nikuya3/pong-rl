5bcd3b323d4aa91bc05aeb18e56d102bcc14c51a
nikuya3 <nikuya@dummy.mail>
2017-10-05 11:50:24 +0200

    Changed smth

--- pong.py
+++ pong.py
@@ -9,6 +9,11 @@ def activation_function(x):
     return x
 
 
+def activation_function_gradient(dx, x):
+    dx[x < 0] = 0
+    return dx
+
+
 def init_weight(in_size, out_size):
     return np.random.randn(in_size, out_size) * sqrt(2 / in_size)
 
@@ -16,8 +21,9 @@ def init_weight(in_size, out_size):
 class PolicyNetwork:
     hidden_layers = []
     weights = []
+    weight_grads = []
 
-    def __init__(self, input_size, hidden_sizes, out_size):
+    def __init__(self, input_size, hidden_sizes, out_size, eta):
         w_in = init_weight(input_size, hidden_sizes[0])
         self.weights.append(w_in)
         for nr in range(1, len(hidden_sizes) - 1):
@@ -26,6 +32,8 @@ class PolicyNetwork:
             self.weights.append(w)
         w_out = init_weight(hidden_sizes[-1], out_size)
         self.weights.append(w_out)
+        self.out_size = out_size
+        self.eta = eta
 
     def forward_pass(self, x):
         h_in = x.dot(self.weights[0])
@@ -38,11 +46,29 @@ class PolicyNetwork:
         scores = self.hidden_layers[-1].dot(self.weights[-1])
         return scores
 
-    def backward_pass(self, dscores):
-        dws = []
-        dw_out = dscores.dot(self.hidden_layers[-1])
+    def backward_pass(self, x, dscores):
+        x = x.reshape(len(x), 1)
+        dscores = dscores.reshape(len(dscores), 1)
+        dw_out = dscores.dot(self.hidden_layers[-1].T)
+        dws = [dw_out]
+        dhiddens = [self.weights[-1].T.dot(dscores)]
+        if len(self.hidden_layers) > 1:
+            for i in range(len(self.hidden_layers) - 1, -1, -1):
+                dhidden = dhiddens[-1].dot(self.weights[i + 1].T)
+                dhidden = activation_function_gradient(dhidden, self.hidden_layers[i])
+                if i == 0:
+                    dw = x.T.dot(dhidden)
+                else:
+                    dw = self.hidden_layers[i - 1].T.dot(dhidden)
+                dhiddens.append(dhidden)
+                dws.append(dw)
+        self.weight_grads.append(dws)
 
-        pass
+    def update_parameters(self):
+        dws = np.array(self.weight_grads)
+        dws = np.average(dws, axis=1)
+        for i in range(len(dws)):
+            self.weights[i] += self.eta * dws[i]
 
 
 def preprocess_image(image):
@@ -101,12 +127,13 @@ def train(env):
     input_size = 80 * 80
     hidden_sizes = [200, 200]
     out_size = 3
-    eps = 1e-4
+    eta = 1e-4
     gamma = 0.99
     rolluts = 20
     actions = [0]
     observations = []
-    net = PolicyNetwork(80 * 80, hidden_sizes, 3)
+    current_gradients = []
+    net = PolicyNetwork(input_size, hidden_sizes, out_size, eta)
     while rolluts > 0:
         action = actions[-1] + 1
         observation, reward, done, info = env.step(action)
@@ -116,10 +143,17 @@ def train(env):
         scores = net.forward_pass(observation)
         # Bias the policy to take the sampled action -> to take clear actions in general
         y = np.argmax(scores)
-        dy = y
+        dscores = np.full(len(scores), -1)
+        dscores[y] = 1
+        current_gradients.append(dscores)
         actions.append(y)
         if done:
-            dy *= reward
+            for i in range(len(current_gradients)):
+                current_gradients[i] = current_gradients[i] * reward
+                net.backward_pass(observations[i], current_gradients[i])
+            net.update_parameters()
+            current_gradients = []
+            observations = []
             rolluts -= 1
             env.reset()
 
